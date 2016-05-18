ba5b0aa02a7783a021c4ad04197bd1ee7c89b025
John Crooks <johnmcrooks@gmail.com>
2016-05-18 11:31:50 -0400

    Start of Text Adventure (welcome/weapon/location)

--- src/com/crooks/Main.java
+++ src/com/crooks/Main.java
@@ -1,8 +1,40 @@
 package com.crooks;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        System.out.println(" Welcome Traveler!");
+        System.out.println("What is your name?\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name);
+
+        System.out.println("Choose your weapon [sword/mace/staff]");
+        String weapon = scanner.nextLine();
+
+        if (weapon.equalsIgnoreCase("sword")){
+            System.out.println("Watch the pointy side, it's dangerous");
+        }else if( weapon.equalsIgnoreCase("mace")){
+            System.out.println("blunt and useful, I dig it.");}
+        else if(weapon.equalsIgnoreCase("staff")){
+            System.out.println("good for the knees on those long journeys");
+        } else{
+            throw new Exception("Yo, Pick a real Weapon");
+        }
+
+        System.out.println("Choose your Location [forest/tunnel]");
+        String location = scanner.nextLine();
+        if (location.equalsIgnoreCase("forest")){
+            System.out.println("entering the dense forests of James Island");
+        } else if (location.equalsIgnoreCase("tunnel")){
+            System.out.println("you have entered the labryinth of sewers deep below charleston");
+        }else{
+            throw new Exception("Invalid Location!");
+        }
+
     }
 }
